d0e9d0ef84ce0b55cf7a6417b16c12416131bd60
quirinof <matheusquinoff@gmail.com>
2025-01-17 23:40:25 -0300

    start recipe next2a

--- app/page.js
+++ app/page.js
@@ -1,24 +1,14 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div>
-      <MenuPrincipal />
-      <Caico />
-    </div>
-  );
-}
-
-export function MenuPrincipal() {
-  return (
-    <div>
-      <div>Menu principal</div>
-    </div>
-  );
-}
-
-export function Caico() {
-  return (
-    <div>
-      <h1>Viva Santana!</h1>
+      <div>
+        <h1>Viva Santana!</h1>
+        <Link href="/novarota">Rota1</Link> <br />
+        <br />
+        <a href="/novarota">Rota 1, jeito antigo</a>
+      </div>
     </div>
   );
 }
